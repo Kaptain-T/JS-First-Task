86b74daa9612a3c1519230206439c1e92063be8e
Kaptain-T <ezighi.antony@gmail.com>
2022-06-24 14:52:59 +0100

    task completed

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("Name: Anthony Ezighi. \n Height: 5.8ft. \n Country: Nigeria")
+alert("My name is Anthony Ezighi")
+alert("I am 5.8ft tall")
+alert("I am from Nigeria")
+
+document.write("<h3>Name: Anthony Ezighi <br> Height: 5.8ft <br> Country: Nigeria<h3>")
